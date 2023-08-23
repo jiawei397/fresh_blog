4d95b8434680ce3aaeddc73aad83b86fef2fdee6
jw397 <jw397@126.com>
2023-08-23 14:51:38 +0800

    perf: 移除不用的文件

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,34 +5,28 @@
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/_app.tsx";
 import * as $2 from "./routes/_middleware.ts";
-import * as $3 from "./routes/api/joke.ts";
-import * as $4 from "./routes/greet/[name].tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/posts.tsx";
-import * as $7 from "./routes/signin.tsx";
-import * as $8 from "./routes/signup.tsx";
-import * as $$0 from "./islands/Counter.tsx";
-import * as $$1 from "./islands/NavSetting.tsx";
-import * as $$2 from "./islands/SigninForm.tsx";
-import * as $$3 from "./islands/SignupForm.tsx";
+import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/posts.tsx";
+import * as $5 from "./routes/signin.tsx";
+import * as $6 from "./routes/signup.tsx";
+import * as $$0 from "./islands/NavSetting.tsx";
+import * as $$1 from "./islands/SigninForm.tsx";
+import * as $$2 from "./islands/SignupForm.tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/_app.tsx": $1,
     "./routes/_middleware.ts": $2,
-    "./routes/api/joke.ts": $3,
-    "./routes/greet/[name].tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/posts.tsx": $6,
-    "./routes/signin.tsx": $7,
-    "./routes/signup.tsx": $8,
+    "./routes/index.tsx": $3,
+    "./routes/posts.tsx": $4,
+    "./routes/signin.tsx": $5,
+    "./routes/signup.tsx": $6,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
-    "./islands/NavSetting.tsx": $$1,
-    "./islands/SigninForm.tsx": $$2,
-    "./islands/SignupForm.tsx": $$3,
+    "./islands/NavSetting.tsx": $$0,
+    "./islands/SigninForm.tsx": $$1,
+    "./islands/SignupForm.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
