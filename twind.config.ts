74b5e55c9cfe30811e96ef5e6c3aee6389a1999a
jw397 <jw397@126.com>
2023-09-01 18:16:24 +0800

    feat: 升级twind到v1版本

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,18 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig, type Preset } from "twind";
+import type { Options } from "$fresh/plugins/twindv1.ts";
+// twind preset
+import presetTailwind from "@twind/preset-tailwind";
+import presetAutoprefix from "@twind/preset-autoprefix";
+
+const config = defineConfig({
+  presets: [
+    presetTailwind() as Preset,
+    presetAutoprefix(),
+  ],
+  preflight: false, // 去掉默认注入的样式
+});
 
 export default {
+  ...config,
   selfURL: import.meta.url,
-  preflight: false, // 去掉默认注入的样式
 } as Options;
